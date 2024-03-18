84d5fc6b0013e864a1939f36b3c05f253b3d279d
ThatOneFrench <petergpt2@gmail.com>
2024-03-18 17:59:24 +0700

    fix

--- json-validator.js
+++ json-validator.js
@@ -1,36 +1,49 @@
-const glob = require('glob');
-const fs = require('fs');
+const fs = require('fs').promises;
+const path = require('path');
 
-function validateJSON(jsonData) {
-  try {
-    var data = JSON.parse(jsonData);
+const requiredProperties = ['age', 'name', 'height', 'company'];
 
-    if (!data.hasOwnProperty('name')) {
-      throw new Error('missing property "name".');
+const findMissingProperties = (json) => {
+  return requiredProperties.filter(prop => !json.hasOwnProperty(prop));
+};
+
+const checkPropertiesRecursively = async (dir) => {
+  const errors = [];
+  const entities = await fs.readdir(dir, { withFileTypes: true });
+
+  for (const entity of entities) {
+    const fullPath = path.join(dir, entity.name);
+    if (entity.isDirectory()) {
+      const subDirErrors = await checkPropertiesRecursively(fullPath);
+      errors.push(...subDirErrors);
+    } else if (entity.isFile() && path.extname(entity.name) === '.json') {
+      try {
+        const data = await fs.readFile(fullPath, 'utf8');
+        const json = JSON.parse(data);
+        const missingProperties = findMissingProperties(json);
+        if (missingProperties.length > 0) {
+          errors.push(`\nFile ${fullPath} missing the following properties: ${missingProperties.join(', ')}.`);
+        }
+      } catch (error) {
+        errors.push(`\Syntax error in ${fullPath}: ${error.message}`);
+      }
     }
-
-    return true;
-  } catch (error) {
-    console.error(error.message);
-    return false;
-  }
-}
-
-
-glob('**/*.json', (err, files) => {
-  if (err) {
-    console.error('Cannot find any .json file', err);
-    process.exit(1);
   }
 
-  files.forEach(file => {
-    const content = fs.readFileSync(file, 'utf8');
-    const isValid = validateJSON(content);
-    if (!isValid) {
-      console.error(`File ${file} is illegal.`);
-      process.exit(1);
+  return errors;
+};
+
+
+const startDirectory = 'json';
+checkPropertiesRecursively(startDirectory)
+  .then(errors => {
+    if (errors.length > 0) {
+      console.log('Issues found:');
+      errors.forEach(error => console.log(error));
+    } else {
+      console.log('All check passed.');
     }
+  })
+  .catch(error => {
+    console.error('A problem occurred while checking files:', error.message);
   });
-
-  console.log('All .json files pass the test.');
-});
